e5394b8ed811c1d2aec8e3054354a9a3f0c7f72a
sk <stefan@bulletproof.net>
2014-04-13 18:46:12 +1000

    the cli consumer of the module

---
+++ passtheword.rb
@@ -0,0 +1,9 @@
+require './hashme.rb'
+
+fail "usage: #{$0} plain-text-password" if $*.empty?
+
+extend Hashme
+message = ""
+$*.each { |string| message << string }
+puts "The password will be in your paste buffer for the next #{self.timeout} seconds. Go!"
+self.hash2clipboard(message, self.salt)
